3d297512f719b543cb942fa5790a646c121888aa
Thiago de Assis <156924136+Mustasheep@users.noreply.github.com>
2024-11-07 13:26:38 -0300

    Update freq_words.r

--- freq_words.r
+++ freq_words.r
@@ -35,10 +35,10 @@ length(corpus)
 inspect(corpus)
 
 # Minerando o corpus
-corpus = tm_map(corpus, content_transformer(tolower)) # Esta linha converte todo o texto do corpus para minúsculas. Isso é crucial para que palavras como "Casa" e "casa" sejam tratadas como iguais, evitando redundância e melhorando a precisão da análise.
-corpus = tm_map(corpus, removePunctuation, ucp = TRUE) # Remove todos os sinais de pontuação do corpus. Pontuação geralmente não carrega significado semântico em análises de texto e pode interferir em alguns algoritmos. Este conjunto inclui vírgulas, pontos, ponto e vírgula, dois pontos, exclamações, interrogações, aspas, parênteses, colchetes, chaves, hífens, sublinhados e apóstrofos.
-corpus = tm_map(corpus, removeNumbers) # Remove todos os números do corpus. Similar à pontuação, números muitas vezes não são relevantes para a análise semântica e podem ser removidos.
-corpus = tm_map(corpus, removeWords, stopwords('portuguese')) # Remove as stop words em português do corpus. Stop words são palavras comuns como "a", "e", "o", "que", "de", etc., que geralmente não carregam muito significado semântico e aparecem com alta frequência. Removê-las reduz o tamanho do corpus e melhora a eficiência do processamento, além de potencialmente melhorar a precisão da análise ao focar em palavras mais relevantes.
+corpus = tm_map(corpus, content_transformer(tolower)) # Esta linha converte todo o texto do corpus para minúsculas.
+corpus = tm_map(corpus, removePunctuation, ucp = TRUE) # Remove todos os sinais de pontuação do corpus.
+corpus = tm_map(corpus, removeNumbers) # Remove todos os números do corpus.
+corpus = tm_map(corpus, removeWords, stopwords('portuguese')) # Remove as stop words em português do corpus.
 corpus = tm_map(corpus, content_transformer(function(x) gsub("\n", "", x)))
 
 # Representação por frequencia de termos
